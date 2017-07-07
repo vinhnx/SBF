4d998f3667ba31d33f09ec733a56748e606ff866
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2017-07-07 11:55:14 +0700

    Initial Commit

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Vinh Nguyen on 7/7/17.
+//  Copyright © 2017 Vinh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
